b64066d69841d4362d31ac506f1326ccfdf16714
root <amolbch>
2016-10-19 11:46:25 +0530

    [Amol|David] Category wise Diseases count report

--- openmrs/apps/reports/customsql/villagewise_patient_count.sql
+++ openmrs/apps/reports/customsql/villagewise_patient_count.sql
@@ -3,6 +3,7 @@ select county_district as District,
 	city_village as Village,
 	count(city_village) as Patient_Count  
 from person_address 
+where date(date_created) BETWEEN  '#startDate#' and '#endDate#'
 group by city_village 
 having count(city_village) > 0 
 order by county_district,address3,city_village;
