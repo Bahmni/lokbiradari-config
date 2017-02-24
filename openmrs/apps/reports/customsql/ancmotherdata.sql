d6c02a305a94ecfa97ed59d6eb72599944171c4d
Amol Chavan <achavan@thoughtworks.com>
2017-02-24 19:16:40 +0530

    Amol | Updated the SQL of ANC Mother data report

--- openmrs/apps/reports/customsql/ancmotherdata.sql
+++ openmrs/apps/reports/customsql/ancmotherdata.sql
@@ -1,13 +1,12 @@
 select * from(
-Select A.person_id,ANC_Mother_Identifier as ANC_Mother_Identifier ,Age,Birthdate,Gender,Tehsil,District,City_Village,max(A.Delivery_date) as Delivery_date,
-max(A.Delivery_Type) as Delivery_Type,A.Mother_Weight as Mother_Weight,max(A.Child_Weight) as Child_Weight from (
+Select ANC_Mother_Identifier as ANC_Mother_Identifier ,Age,Birthdate,Tehsil,District,City_Village,max(A.Delivery_date) as Delivery_date,
+max(A.Delivery_Type) as Delivery_Type,A.Mother_Weight as Mother_Weight,max(A.Child_Weight) as Child_Weight, max(A.Child_Gender) as Child_Gender from (
 SELECT
 o.person_id,
   pi.identifier AS 'ANC_Mother_Identifier',
   concat(pn.given_name, " ", pn.family_name)                    AS 'Patient Name',
   floor(DATEDIFF(DATE(o.obs_datetime), p.birthdate) / 365)      AS 'Age',
   DATE_FORMAT(p.birthdate, "%d-%b-%Y")                          AS 'Birthdate',
-  p.gender                                                      AS 'Gender',
   paddress.city_village                                            AS 'City_Village', 
   paddress.address3                                                AS 'Tehsil', 
 paddress.county_district AS 'District',
@@ -15,7 +14,8 @@ paddress.county_district AS 'District',
  date(GROUP_CONCAT(DISTINCT(IF(obs_fscn.name = 'Date of Birth', coalesce(o.value_numeric, o.value_boolean, o.value_text, o.value_datetime, coded_scn.name, coded_fscn.name), NULL)) ORDER BY o.obs_id DESC))AS 'Delivery_date',
   GROUP_CONCAT(DISTINCT(IF(obs_fscn.name = 'Delivery, Type', coalesce(o.value_numeric, o.value_boolean, o.value_text, o.value_datetime, coded_scn.name, coded_fscn.name), NULL)) ORDER BY o.obs_id DESC) AS 'Delivery_Type',
  case when o.form_namespace_and_path='6aa2c0e0-7c0f-4f44-b281-33e57b7595be^' then GROUP_CONCAT(DISTINCT(IF(obs_fscn.name = 'Weight', coalesce(o.value_numeric, o.value_boolean, o.value_text, o.value_datetime, coded_scn.name, coded_fscn.name), NULL)) ORDER BY o.obs_id DESC)end as 'Mother_Weight',
-  case when o.form_namespace_and_path='a02c4673-bff7-4f32-a28a-f39c34687868^' then GROUP_CONCAT(DISTINCT(IF(obs_fscn.name = 'Weight', coalesce(o.value_numeric, o.value_boolean, o.value_text, o.value_datetime, coded_scn.name, coded_fscn.name), NULL)) ORDER BY o.obs_id DESC)end as 'Child_Weight'	
+  case when o.form_namespace_and_path='a02c4673-bff7-4f32-a28a-f39c34687868^' then GROUP_CONCAT(DISTINCT(IF(obs_fscn.name = 'Weight', coalesce(o.value_numeric, o.value_boolean, o.value_text, o.value_datetime, coded_scn.name, coded_fscn.name), NULL)) ORDER BY o.obs_id DESC)end as 'Child_Weight',
+(GROUP_CONCAT(DISTINCT(IF(obs_fscn.name = 'Gender', coalesce(o.value_numeric, o.value_boolean, o.value_text, o.value_datetime, coded_scn.name, coded_fscn.name), NULL)) ORDER BY o.obs_id DESC))AS 'Child_Gender'
   FROM obs o
   JOIN concept obs_concept ON obs_concept.concept_id=o.concept_id AND obs_concept.retired is false
   JOIN concept_name obs_fscn on o.concept_id=obs_fscn.concept_id AND obs_fscn.concept_name_type="FULLY_SPECIFIED" AND obs_fscn.voided is false
@@ -42,6 +42,4 @@ paddress.county_district AS 'District',
   LEFT OUTER JOIN person_address paddress ON p.person_id = paddress.person_id AND paddress.voided is false 
   WHERE o.voided is false  
   AND cast(o.obs_datetime AS DATE) between '#startDate#'AND '#endDate#' GROUP BY e.encounter_id 
-  ) as A group by A.ANC_Mother_Identifier)as sample where sample.Child_Weight>0
-  
-  
+  ) as A group by A.ANC_Mother_Identifier)as sample where sample.Child_Weight>0;
